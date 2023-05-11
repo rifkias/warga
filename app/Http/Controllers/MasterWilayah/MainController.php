8c0aef43690d7a6e1ce5c42969ca3b746e03a676
rifkias <rifkialfarizshidiq.1@gmail.com>
2023-05-11 07:11:22 +0700

    Fix Wilayah Select2 & Add Village Select2

--- app/Http/Controllers/MasterWilayah/MainController.php
+++ app/Http/Controllers/MasterWilayah/MainController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\MasterWilayah\Province;
 use App\Models\MasterWilayah\City;
 use App\Models\MasterWilayah\District;
+use App\Models\MasterWilayah\Village;
 
 class MainController extends Controller
 {
@@ -35,10 +36,21 @@ class MainController extends Controller
     {
         $datas = New District;
         if($request->has('city_id')){
-            $datas->where('city_id',$request->city_id);
+            $datas = $datas->where('city_id',$request->city_id);
         }
         if($request->has('name')){
             $datas = $datas->where('district_name',"like","%".$request->name."%");
+        }
+        return $datas->take(10)->get();
+    }
+    public function getVillage(Request $request)
+    {
+        $datas = New Village;
+        if($request->has('district_id')){
+            $datas =  $datas->where('district_id',$request->district_id);
+        }
+        if($request->has('name')){
+            $datas = $datas->where('village_name',"like","%".$request->name."%");
         }
         return $datas->take(10)->get();
     }
